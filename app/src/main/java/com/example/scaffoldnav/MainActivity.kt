0fd1b6ad61ef6ee698b1be1f6638b5c6599da0bc
m3na0 <m3na02@gmail.com>
2024-02-16 21:03:09 +0200

    Adding TopBar and Icons like menu and morevert

--- app/src/main/java/com/example/scaffoldnav/MainActivity.kt
+++ app/src/main/java/com/example/scaffoldnav/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.scaffoldnav
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,30 +27,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ScaffoldnavTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                    ScaffoldApp()
             }
         }
     }
 }
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ScaffoldApp() {
+    Scaffold(
+        topBar = { MyTopBar()},
+        content = { Text(text = "Content for Home Screen") },
+        //bottomBar = { BottomAppBar(content = { Text(text = "Bottom bar") }) }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar(){
+    TopAppBar(title = { Text("My App")},
+    navigationIcon ={
+        IconButton(onClick = { /*TODO*/ },
+            ) {
+                Icon(Icons.Filled.Menu, contentDescription = null)
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }
+            ) {
+                Icon(Icons.Filled.MoreVert, contentDescription = null)
+            }
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     ScaffoldnavTheme {
-        Greeting("Android")
+        ScaffoldApp()
     }
 }
