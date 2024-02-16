3e00bcf64f8a0717e0b1be6e1c03fa9d8005cce8
m3na0 <m3na02@gmail.com>
2024-02-16 21:26:39 +0200

    Added a dropdownmenu like info and settings

--- app/src/main/java/com/example/scaffoldnav/MainActivity.kt
+++ app/src/main/java/com/example/scaffoldnav/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,6 +20,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.scaffoldnav.ui.theme.ScaffoldnavTheme
@@ -48,21 +54,29 @@ fun ScaffoldApp() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopBar(){
-    TopAppBar(title = { Text("My App")},
-    navigationIcon ={
-        IconButton(onClick = { /*TODO*/ },
-            ) {
+    var expanded by remember { mutableStateOf(false) }
+
+    TopAppBar(title = { Text(text = "My App") },
+        navigationIcon ={
+            IconButton(onClick = { /*TODO*/ }) {
                 Icon(Icons.Filled.Menu, contentDescription = null)
             }
         },
         actions = {
-            IconButton(onClick = { /*TODO*/ }
-            ) {
+            IconButton(onClick = { expanded = !expanded}) {
                 Icon(Icons.Filled.MoreVert, contentDescription = null)
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                DropdownMenuItem(text = { Text(text = "Info") }, onClick = { /*TODO*/ })
+                DropdownMenuItem(text = { Text(text = "Settings") }, onClick = { /*TODO*/ })
             }
         }
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
